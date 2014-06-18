71a0d8440c12d6eca90525e2aa58c06e047c69db
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-06-18 14:51:36 +0100

    enable/disable

--- andlog.js
+++ andlog.js
@@ -4,14 +4,20 @@
         ls = !inNode && window.localStorage,
         out = {};
 
+    var options = {
+        enabled: true
+    }
+    
     if (inNode) {
         module.exports = console;
         return;
     }
 
     if (ls && ls.debug && window.console) {
+        options.enabled = true;
         out = window.console;
     } else {
+        options.enabled = false;
         var methods = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),
             l = methods.length,
             fn = function () {};
